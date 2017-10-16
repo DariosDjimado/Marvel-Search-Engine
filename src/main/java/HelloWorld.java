38ae8daf513fb095fce397d6a5a96f044069a846
Théo Basty <theo.basty@gmail.com>
2017-10-16 17:48:29 +0200

    Init repo

---
+++ src/main/java/HelloWorld.java
@@ -0,0 +1,11 @@
+import org.joda.time.LocalTime;
+
+/**
+ * Created by teteo on 16/10/2017.
+ */
+public class HelloWorld {
+    public static void main(String args[]){
+        LocalTime currentTime = new LocalTime();
+        System.out.println("time="+currentTime);
+    }
+}
