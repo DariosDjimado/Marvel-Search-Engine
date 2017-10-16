61b38d3243cd796e7154bf4b2db0a5fe8049234c
DariosDjimado <dariosdjimado>
2017-10-16 18:09:10 +0200

    hello world

---
+++ src/main/java/heapoverflow/HelloWorld.java
@@ -0,0 +1,10 @@
+package heapoverflow;
+
+import org.joda.time.LocalTime;
+
+public class HelloWorld {
+	public static void main(String[] args) {
+		LocalTime currentTime= new LocalTime();
+		System.out.println("time = " + currentTime);
+	}
+}
