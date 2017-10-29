c4ea35197a8eb1a6ca731f6dfa2d2580b0d57ca6
Théo Basty <theo.basty@gmail.com>
2017-10-29 21:11:28 +0100

    Creating a common class to share UI fonts

---
+++ src/main/java/heapoverflow/display/Fonts.java
@@ -0,0 +1,13 @@
+package heapoverflow.display;
+
+import java.awt.*;
+
+/**
+ * Class containing fonts definitions for UI
+ * @author Théo Basty
+ */
+public class Fonts {
+    public static Font title1 = new Font("Segoe UI", Font.BOLD, 40);
+    public static Font title2 = new Font("Segoe UI", Font.BOLD, 30);
+    public static Font content = new Font("Segoe UI", Font.PLAIN, 10);
+}
