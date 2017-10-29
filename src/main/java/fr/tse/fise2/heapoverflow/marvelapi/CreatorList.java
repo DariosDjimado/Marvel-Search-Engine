ed52b27f7b41eaf48268de2ab76c4a200529122e
djicodas <comlanvi-darios.djimado@telecom-st-etienne.fr>
2017-10-29 10:53:21 +0100

    add CreatorList class

---
+++ src/main/java/fr/tse/fise2/heapoverflow/marvelapi/CreatorList.java
@@ -0,0 +1,26 @@
+package fr.tse.fise2.heapoverflow.marvelapi;
+
+import java.util.Arrays;
+
+public class CreatorList extends TemplateList {
+
+    private CreatorSummary[] items;
+
+    public CreatorSummary[] getItems() {
+        return items;
+    }
+
+    public void setItems(CreatorSummary[] items) {
+        this.items = items;
+    }
+
+    @Override
+    public String toString() {
+        return "CreatorList{" +
+                "items=" + Arrays.toString(items) +
+                ", collectionURI='" + collectionURI + '\'' +
+                ", available='" + available + '\'' +
+                ", returned='" + returned + '\'' +
+                '}';
+    }
+}
