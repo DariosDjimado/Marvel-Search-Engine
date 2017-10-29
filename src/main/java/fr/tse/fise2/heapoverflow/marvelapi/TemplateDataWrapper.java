2434d223b7b81ba7cb6533f1545a2678ef8afa43
djicodas <comlanvi-darios.djimado@telecom-st-etienne.fr>
2017-10-29 10:34:21 +0100

    add a base POJO of all wrapper
    since we know thtat all *.DataWrapper have some fields in common, i have
    created a base POJO with protected field for them. This POJO will be
    extended by all *.DataWrapper. It doesn't contain any override we will
    need to override toString() method as needed.

---
+++ src/main/java/fr/tse/fise2/heapoverflow/marvelapi/TemplateDataWrapper.java
@@ -0,0 +1,67 @@
+package fr.tse.fise2.heapoverflow.marvelapi;
+
+/**
+ * This class consists exclusively of  public methods that return or set protected fields. The fields are common of all
+ * class *.DataWrapper. This POJO doesn't contain the override toString method.
+ */
+public class TemplateDataWrapper {
+    protected String attributionText;
+
+    protected String etag;
+
+    protected String status;
+
+    protected String copyright;
+
+    protected String code;
+
+    protected String attributionHTML;
+
+    public String getAttributionText() {
+        return attributionText;
+    }
+
+    public void setAttributionText(String attributionText) {
+        this.attributionText = attributionText;
+    }
+
+    public String getEtag() {
+        return etag;
+    }
+
+    public void setEtag(String etag) {
+        this.etag = etag;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getCopyright() {
+        return copyright;
+    }
+
+    public void setCopyright(String copyright) {
+        this.copyright = copyright;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getAttributionHTML() {
+        return attributionHTML;
+    }
+
+    public void setAttributionHTML(String attributionHTML) {
+        this.attributionHTML = attributionHTML;
+    }
+}
