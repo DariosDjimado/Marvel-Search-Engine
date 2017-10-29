335b920fcdd5fafc7bcc5c8d274a68fb2d441a32
Théo Basty <theo.basty@gmail.com>
2017-10-29 23:47:59 +0100

    display comic title in window

--- src/main/java/fr/tse/fise2/heapoverflow/display/DataShow.java
+++ src/main/java/fr/tse/fise2/heapoverflow/display/DataShow.java
@@ -28,15 +28,15 @@ public class DataShow extends JFrame {
      */
     public DataShow(Comic comic) {
         this.setTitle(comic.getTitle());
-        this.setSize(400, 500);
+        this.setSize(1000, 500);
         this.setLocationRelativeTo(null);
         this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 
         this.setLayout(new BorderLayout());
 
         Title1Panel head = new Title1Panel(comic.getTitle());
-        head.setLayout(new BoxLayout(head, BoxLayout.LINE_AXIS));
-        this.getContentPane().add(head, BorderLayout.PAGE_START);
+        head.setPreferredSize(new Dimension(head.getFontMetrics(Fonts.title1).stringWidth(comic.getTitle()), 45));
+        this.getContentPane().add(head, BorderLayout.NORTH);
 
         this.setVisible(true);
     }
