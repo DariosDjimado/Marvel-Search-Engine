6d977424316f6dd6a328f44b23ef32b6df756477
djicodas <comlanvi-darios.djimado@telecom-st-etienne.fr>
2017-10-29 10:50:57 +0100

    add ComicDataWrapper class

---
+++ src/main/java/fr/tse/fise2/heapoverflow/marvelapi/ComicDataWrapper.java
@@ -0,0 +1,27 @@
+package fr.tse.fise2.heapoverflow.marvelapi;
+
+public class ComicDataWrapper extends TemplateDataWrapper {
+
+    private ComicDataContainer data;
+
+    public ComicDataContainer getData() {
+        return data;
+    }
+
+    public void setData(ComicDataContainer data) {
+        this.data = data;
+    }
+
+    @Override
+    public String toString() {
+        return "ComicDataWrapper{" +
+                "data=" + data +
+                ", attributionText='" + attributionText + '\'' +
+                ", etag='" + etag + '\'' +
+                ", status='" + status + '\'' +
+                ", copyright='" + copyright + '\'' +
+                ", code='" + code + '\'' +
+                ", attributionHTML='" + attributionHTML + '\'' +
+                '}';
+    }
+}
