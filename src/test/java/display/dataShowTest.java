6ef2a90cbe9fed3737da53c9c030e4f9477f3381
theo.basty <theo.basty@gmail.com>
2017-10-31 17:13:02 +0100

    Edit test to get ressource from API

--- src/test/java/display/dataShowTest.java
+++ src/test/java/display/dataShowTest.java
@@ -15,11 +15,11 @@ import static fr.tse.fise2.heapoverflow.marvelapi.MarvelRequest.deserializeComic
 public class dataShowTest {
     public static void main(String[] args) {
         MarvelRequest request = new MarvelRequest();
-        Path myFile = Paths.get("comicSample.json");
+//        Path myFile = Paths.get("comicSample.json");
         try {
-//            String response = request.getData("comics/64785");
+            String response = request.getData("comics/64785");
 //            Files.write(myFile, Arrays.asList(response));
-            String response = Files.readAllLines(myFile).get(0);
+//            String response = Files.readAllLines(myFile).get(0);
             Comic fetched = deserializeComics(response).getData().getResults()[0];
             DataShow testWindow = new DataShow(fetched);
         }
