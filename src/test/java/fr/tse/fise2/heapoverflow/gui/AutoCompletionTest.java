49cda3b6286090b1378e4948208a8907297a174f
LionelRajaona <lionel.rajaona@github.com>
2017-11-06 17:54:55 +0100

    Autocompletion class, with tests. I made a simple GUI that has to be merged with Searching part (Ismail).

---
+++ src/test/java/fr/tse/fise2/heapoverflow/gui/AutoCompletionTest.java
@@ -0,0 +1,59 @@
+package fr.tse.fise2.heapoverflow.gui;
+import fr.tse.fise2.heapoverflow.database.DataBase;
+import javax.swing.*;
+import java.awt.*;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Comparator;
+import static org.junit.Assert.*;
+
+public class AutoCompletionTest {
+    public AutoCompletionTest() {
+        JFrame frame = new JFrame();
+        frame.setPreferredSize(new Dimension(300,400));
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+
+        JTextField f = new JTextField(20);
+
+        AutoCompletion autoCompletion = new AutoCompletion(f, frame, null, Color.WHITE.brighter(), Color.BLUE, Color.RED, 075f) {
+            @Override
+            boolean wordTyped(String typedWord) {
+
+                //create list for dictionary this in your case might be done via calling a method which queries db and returns results as arraylist
+                ArrayList<String> words = new ArrayList<>();
+
+                try {
+                    for(String aComicName: DataBase.getComicNames()){
+                        words.add(aComicName.toLowerCase());
+                    }
+                    words.sort(Comparator.naturalOrder());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+                setDictionary(words);
+
+                return super.wordTyped(typedWord);//now call super to check for any matches against newest dictionary
+            }
+        };
+
+        JPanel p = new JPanel();
+
+        p.add(f);
+
+        frame.add(p);
+
+        frame.pack();
+        frame.setVisible(true);
+    }
+    //How this class works
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                new AutoCompletionTest();
+            }
+        });
+    }
+
+}
