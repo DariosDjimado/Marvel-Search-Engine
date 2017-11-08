5006bcbf3176505d2d1a54cfcff421102401f98f
theo.basty <theo.basty@gmail.com>
2017-11-08 21:57:24 +0100

    move dataShow example to the right package

--- src/test/java/fr/tse/fise2/heapoverflow/gui/dataShowTestCharacter.java
+++ src/test/java/fr/tse/fise2/heapoverflow/gui/dataShowTestCharacter.java
@@ -1,23 +1,22 @@
-package display;
+package fr.tse.fise2.heapoverflow.gui;
 
 import fr.tse.fise2.heapoverflow.gui.DataShow;
-import fr.tse.fise2.heapoverflow.marvelapi.Comic;
+import fr.tse.fise2.heapoverflow.marvelapi.Character;
 import fr.tse.fise2.heapoverflow.marvelapi.MarvelRequest;
 
-import static fr.tse.fise2.heapoverflow.marvelapi.MarvelRequest.deserializeComics;
+import static fr.tse.fise2.heapoverflow.marvelapi.MarvelRequest.deserializeCharacters;
 
-public class dataShowTestComic {
+public class dataShowTestCharacter {
     public static void main(String[] args) {
         MarvelRequest request = new MarvelRequest();
 //        Path myFile = Paths.get("comicSample.json");
         try {
-            String response = request.getData("comics/65081");
+            String response = request.getData("characters/1009367");
 //            Files.write(myFile, Arrays.asList(response));
 //            String response = Files.readAllLines(myFile).get(0);
-            Comic fetched = deserializeComics(response).getData().getResults()[0];
+            Character fetched = deserializeCharacters(response).getData().getResults()[0];
             DataShow testWindow = new DataShow(fetched);
-        }
-        catch(Exception e)  {
+        } catch (Exception e) {
             System.out.println(e);
         }
 
--- src/test/java/fr/tse/fise2/heapoverflow/gui/dataShowTestComic.java
+++ src/test/java/fr/tse/fise2/heapoverflow/gui/dataShowTestComic.java
@@ -1,22 +1,23 @@
-package display;
+package fr.tse.fise2.heapoverflow.gui;
 
 import fr.tse.fise2.heapoverflow.gui.DataShow;
-import fr.tse.fise2.heapoverflow.marvelapi.Character;
+import fr.tse.fise2.heapoverflow.marvelapi.Comic;
 import fr.tse.fise2.heapoverflow.marvelapi.MarvelRequest;
 
-import static fr.tse.fise2.heapoverflow.marvelapi.MarvelRequest.deserializeCharacters;
+import static fr.tse.fise2.heapoverflow.marvelapi.MarvelRequest.deserializeComics;
 
-public class dataShowTestCharacter {
+public class dataShowTestComic {
     public static void main(String[] args) {
         MarvelRequest request = new MarvelRequest();
 //        Path myFile = Paths.get("comicSample.json");
         try {
-            String response = request.getData("characters/1009367");
+            String response = request.getData("comics/65081");
 //            Files.write(myFile, Arrays.asList(response));
 //            String response = Files.readAllLines(myFile).get(0);
-            Character fetched = deserializeCharacters(response).getData().getResults()[0];
+            Comic fetched = deserializeComics(response).getData().getResults()[0];
             DataShow testWindow = new DataShow(fetched);
-        } catch (Exception e) {
+        }
+        catch(Exception e)  {
             System.out.println(e);
         }
 
