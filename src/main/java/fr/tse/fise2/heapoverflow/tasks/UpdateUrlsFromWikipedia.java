558ff1ca14194e28873d48420ef567157eb784d7
djicodas <comlanvi-darios.djimado@telecom-st-etienne.fr>
2017-11-20 21:43:44 +0100

    use wikidata api instead of custom fetch

--- src/main/java/fr/tse/fise2/heapoverflow/tasks/UpdateUrlsFromWikipedia.java
+++ src/main/java/fr/tse/fise2/heapoverflow/tasks/UpdateUrlsFromWikipedia.java
@@ -4,6 +4,9 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
+import org.wikidata.wdtk.datamodel.interfaces.ItemDocument;
+import org.wikidata.wdtk.wikibaseapi.WikibaseDataFetcher;
+import org.wikidata.wdtk.wikibaseapi.apierrors.MediaWikiApiErrorException;
 
 import java.io.IOException;
 
@@ -12,13 +15,19 @@ import java.io.IOException;
  * @author Darios DJIMADO
  */
 public class UpdateUrlsFromWikipedia {
+    static int count = 0;
+
     public static void main(String[] args) {
+
+
         UpdateUrlsFromWikipedia updateUrlsFromWikipedia = new UpdateUrlsFromWikipedia();
         try {
             String url = updateUrlsFromWikipedia.getEnUrls("https://en.wikipedia.org/wiki/Category:Marvel_Comics_superheroes");
             while (url != null) {
                 url = updateUrlsFromWikipedia.getEnUrls(url);
             }
+
+            System.out.println(count);
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -28,6 +37,9 @@ public class UpdateUrlsFromWikipedia {
 
     private String getEnUrls(String url) throws IOException {
         String nextPage = null;
+
+        WikibaseDataFetcher wbdf = WikibaseDataFetcher.getWikidataDataFetcher();
+
 
         // handle next page href attributes
         if (url.startsWith("/w/")) {
@@ -39,8 +51,24 @@ public class UpdateUrlsFromWikipedia {
         Elements uls = mainPage.getElementsByTag("ul");
         for (Element ul : uls) {
             for (Element li : ul.getElementsByTag("li")) {
-                System.out.println(li.text() + "    --->     https://en.wikipedia.org" +
-                        li.select("a").attr("href"));
+                count++;
+
+                try {
+                    ItemDocument itemDocument = (ItemDocument) wbdf.getEntityDocumentByTitle("enwiki", li.text());
+
+                    System.out.println(li.text() + "    --->     https://en.wikipedia.org" +
+                            li.select("a").attr("href"));
+                    if(itemDocument!=null){
+                        System.out.println("-->  " + itemDocument.getLabels().get("en") + "-->  " + itemDocument.getAliases().get("en"));
+                    } else{
+                        System.out.println(li.text());
+                    }
+
+                } catch (MediaWikiApiErrorException e) {
+                    e.printStackTrace();
+                }
+
+
             }
         }
 
