eda80448b8973399338b03713c18d7b15d0cfa18
ismail-berrada <ismai-berrada@live.fr>
2017-11-21 23:09:57 +0100

    add ui main component

---
+++ src/main/java/fr/tse/fise2/heapoverflow/gui/UIMainComponent.java
@@ -0,0 +1,21 @@
+package fr.tse.fise2.heapoverflow.gui;
+
+import fr.tse.fise2.heapoverflow.interfaces.UIComponent;
+
+public class UIMainComponent implements UIComponent {
+
+    @Override
+    public void setSize() {
+
+    }
+
+    @Override
+    public void build() {
+
+    }
+
+    @Override
+    public void setVisible() {
+
+    }
+}
